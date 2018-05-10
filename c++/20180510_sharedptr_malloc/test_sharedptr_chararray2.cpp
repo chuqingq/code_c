e2eb0ed0a2241336e6d44b667a165e125890f74a
chuqq <chuqingq@qq.com>
2018-05-10 14:54:50 +0800

    unique_ptr

--- c++/20180510_sharedptr_malloc/test_sharedptr_chararray2.cpp
+++ c++/20180510_sharedptr_malloc/test_sharedptr_chararray2.cpp
@@ -1,10 +1,16 @@
 #include <memory>
+#include <functional>
 #include <stdlib.h>
 
 int main() {
   {
     std::shared_ptr<char> a((char*)malloc(10), free);
     (void)a;
+  }
+
+  {
+    std::unique_ptr<char, std::function<void(char *)> > b((char*)malloc(10), free);
+    (void)b;
   }
 
     return 0;
