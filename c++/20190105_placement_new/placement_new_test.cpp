b5a5abdd79dc128355fc2f675c0976d52a26099f
chuqq <chuqingq@qq.com>
2019-02-04 01:23:02 +0800

    placement_new补充说明

--- c++/20190105_placement_new/placement_new_test.cpp
+++ c++/20190105_placement_new/placement_new_test.cpp
@@ -9,8 +9,8 @@ class Book {
 int main() {
     // char array[sizeof(Book)]; // 不要14行的释放，不会有内存泄漏
     char *array = new char[sizeof(Book)];
-    Book *b = new(array) Book();
-    b->~Book();
+    Book *b = new(array) Book(); // 使用array这块内存来创建Book对象
+    b->~Book(); // 析构，但是不会释放array的空间
     delete[] array; // 加上这行，valgrind仍然有内存泄漏，应该是valgrind误报
     return 0;
 }
