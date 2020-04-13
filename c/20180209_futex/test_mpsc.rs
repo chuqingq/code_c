6bff7c327ae4cfc39daa7cc3282f78804370ce67
chuqq <chuqingq@qq.com>
2020-04-13 21:52:36 +0800

    rust.mpsc: 120us

--- c/20180209_futex/test_mpsc.rs
+++ c/20180209_futex/test_mpsc.rs
@@ -1,18 +1,20 @@
 use std::{thread, time};
-use std::sync::mpsc::sync_channel;
+use std::sync::mpsc::channel;
 
 fn main() {
 // Create a simple streaming channel
-let (tx, rx) = sync_channel(0);
+let (tx, rx) = channel();
 
 thread::spawn(move|| {
+let ten_millis = time::Duration::from_millis(1000);
+thread::sleep(ten_millis);
+
+
     let now = time::Instant::now();
     println!("sender: {:?}", now);
     tx.send(10).unwrap();
 });
 
-let ten_millis = time::Duration::from_millis(100);
-thread::sleep(ten_millis);
 
 let r = rx.recv().unwrap();
 let now = time::Instant::now();
