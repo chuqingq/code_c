77f66668152177e68d389eef41e8e90038d0fd0b
chuqq <chuqingq@qq.com>
2020-11-14 00:21:38 +0800

    usleep或者this_thread::sleep_for都可以到3ms级别

--- c++/20201113-duration-ms-us/mstime.cpp
+++ c++/20201113-duration-ms-us/mstime.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+
+#include <sys/time.h>
+#include <unistd.h>
  
 void f()
 {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // std::this_thread::sleep_for(std::chrono::milliseconds(3));
+	usleep(3000);
 }
 
-long mstime() {
+static long mstime() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+}
+
+static unsigned long long ustime(void) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return ((unsigned long long)tv.tv_sec)*1000000 + tv.     tv_usec;           
 }
 
 int main()
 {
-    auto start = mstime();
+    // auto start = mstime();
+	auto start = ustime();
     f();
-    auto end = mstime();
+    // auto end = mstime();
+	auto end = ustime();
 
     std::cout << "f() took " << end-start << " ms, " << std::endl;
 }
