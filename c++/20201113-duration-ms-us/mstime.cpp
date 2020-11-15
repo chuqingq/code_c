710f6874420eaee2ab92b8f445b746364820576e
chuqq <chuqingq@qq.com>
2020-11-15 14:39:17 +0800

    ustime

--- c++/20201113-duration-ms-us/mstime.cpp
+++ c++/20201113-duration-ms-us/mstime.cpp
@@ -15,19 +15,27 @@ static long mstime() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 }
 
-static unsigned long long ustime(void) {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return ((unsigned long long)tv.tv_sec)*1000000 + tv.     tv_usec;           
+static long long ustime() {
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 }
+
+
+// static unsigned long long ustime(void) {
+//     struct timeval tv;
+//     gettimeofday(&tv, NULL);
+//     return ((unsigned long long)tv.tv_sec)*1000000 + tv.     tv_usec;           
+// }
 
 int main()
 {
     // auto start = mstime();
 	auto start = ustime();
+    auto start1 = mstime();
     f();
     // auto end = mstime();
 	auto end = ustime();
+    auto end1 = mstime();
 
-    std::cout << "f() took " << end-start << " ms, " << std::endl;
+    std::cout << "f() took " << end1-start1 << " ms, " << std::endl;
+    std::cout << "f() took " << end-start << " us, " << std::endl;
 }
