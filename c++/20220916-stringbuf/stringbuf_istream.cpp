e3591db70a6a4592d4ba2454fcd99542b4753db6
chuqq <chuqingq@qq.com>
2022-09-16 16:17:58 +0800

    stringbuf in istream will eof

---
+++ c++/20220916-stringbuf/stringbuf_istream.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include <unistd.h>
+
+using namespace std;
+
+stringbuf buf("", ios_base::in | ios_base::out | ios_base::app);
+
+// produce
+void f1() {
+	for (int i = 0; i < 10000; i++) {
+		buf.sputc('0' + (i%10));
+		if ((i%10) == 9) {
+			buf.sputc('\n');
+		}
+		usleep(100);
+	}
+}
+
+// consume
+void f2() {
+	istream is(&buf);
+	string str;
+	while (1) {
+		str = "";
+		if (is.eof()) {
+			is.clear(ios_base::eofbit);
+			is.clear();
+			// printf("consume eof\n");
+		}
+		getline(is, str);
+		// cout << "consume: " << &buf << "\n";
+		cout << "consume: " << str << "\n";
+		usleep(10000);
+	}
+}
+
+int main() {
+	thread t1(f1);
+	thread t2(f2);
+	t1.join();
+	t2.join();
+	return 0;
+}
+
