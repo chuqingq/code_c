6d8d074dab0cd8fb7cff2283a9a05e00583f3ebf
chuqq <chuqingq@qq.com>
2022-09-24 19:34:33 +0800

    c++20 coroutine

---
+++ c++/20220924-c++-coroutine/coroutine.cpp
@@ -0,0 +1,54 @@
+#include <concepts>
+#include <coroutine>
+#include <exception>
+#include <iostream>
+
+struct ReturnObject {
+  struct promise_type {
+    ReturnObject get_return_object() { return {}; }
+    std::suspend_never initial_suspend() { return {}; }
+    std::suspend_never final_suspend() noexcept { return {}; }
+    void unhandled_exception() {}
+  };
+};
+
+struct Awaiter {
+  std::coroutine_handle<> *hp_;
+  constexpr bool await_ready() const noexcept { return false; }
+  void await_suspend(std::coroutine_handle<> h) { *hp_ = h; }
+  constexpr void await_resume() const noexcept {}
+};
+
+ReturnObject
+counter(std::coroutine_handle<> *continuation_out)
+{
+  Awaiter a{continuation_out};
+  for (unsigned i = 0;; ++i) {
+    co_await a;
+    std::cout << "counter: " << i << std::endl;
+  }
+}
+
+int
+main()
+{
+  std::coroutine_handle<> h;
+  counter(&h);
+  for (int i = 0; i < 3; ++i) {
+    std::cout << "In main1 function\n";
+    h();
+  }
+  h.destroy();
+  return 0;
+}
+
+/*
+chuqq@chuqq-r7000p/m/d/t/p/c/c/20220924-c++-coroutine $ g++ -o coroutine -std=c++20 coroutine.cpp
+chuqq@chuqq-r7000p/m/d/t/p/c/c/20220924-c++-coroutine $ ./coroutine
+In main1 function
+counter: 0
+In main1 function
+counter: 1
+In main1 function
+counter: 2
+*/
