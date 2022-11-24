5794e2028468c6b5c61d9bcf76472a558a344daa
chuqq <chuqingq@qq.com>
2022-11-24 22:47:09 +0800

    rename ScopeGuard to scope_guard

--- util/scope_guard_test.cpp
+++ util/scope_guard_test.cpp
@@ -1,4 +1,4 @@
-#include "ScopeGuard.hpp"
+#include "scope_guard.hpp"
 
 #include <iostream>
 using namespace std;
@@ -12,25 +12,22 @@ int main()
 
     // 普通用法
     {
-        ScopeGuard a([&] {
-            i++;
-        });
+        ScopeGuard a([&]
+                     { i++; });
     }
     assert(i == 1);
 
     // 匿名
     {
-        ScopeGuard([&] {
-            i++;
-        });
+        ScopeGuard([&]
+                   { i++; });
     }
     assert(i == 2);
 
     // dismiss
     {
-        ScopeGuard c([&] {
-            i = 3;
-        });
+        ScopeGuard c([&]
+                     { i = 3; });
         c.dismiss();
     }
     assert(i == 2);
