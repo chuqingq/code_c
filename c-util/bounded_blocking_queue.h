b93235441791593ca2da738b656050b5de822712
chuqq <chuqingq@qq.com>
2022-11-25 20:26:11 +0800

    queue_close()

--- c-util/bounded_blocking_queue.h
+++ c-util/bounded_blocking_queue.h
@@ -7,53 +7,53 @@
 typedef struct
 {
     size_t size;
-    size_t head; // 可以get的位置
-    size_t tail; // 可以put的位置
+    size_t out; // 可以get的位置
+    size_t in;  // 可以put的位置
     int closed;
     void **data;
     pthread_mutex_t lock;
-    pthread_cond_t readable;
-    pthread_cond_t writeable;
+    pthread_cond_t not_empty; // 变为不空
+    pthread_cond_t not_full;  // 变为不满
 } queue_t;
 
-static int queue_init(queue_t *q, size_t size)
+static inline int queue_init(queue_t *q, size_t size)
 {
-    if (q == NULL || size <= 1)
+    if (q == NULL || size <= 0)
     {
         return -1;
     }
 
-    q->size = size;
-    q->head = 0;
-    q->tail = 0;
+    q->size = size + 1; // 多留一个空位用于区分full和empty
+    q->out = 0;
+    q->in = 0;
     q->closed = 0;
-    q->data = (void **)malloc(sizeof(*q->data) * size);
+    q->data = (void **)malloc(sizeof(*q->data) * q->size);
     if (q->data == NULL)
     {
         return -1;
     }
 
     pthread_mutex_init(&q->lock, NULL);
-    pthread_cond_init(&q->readable, NULL);
-    pthread_cond_init(&q->writeable, NULL);
+    pthread_cond_init(&q->not_empty, NULL);
+    pthread_cond_init(&q->not_full, NULL);
 
     return 0;
 }
 
-static void queue_close(queue_t *q)
+static inline void queue_close(queue_t *q)
 {
     pthread_mutex_lock(&q->lock);
     q->closed = 1;
     pthread_mutex_unlock(&q->lock);
 
-    pthread_cond_broadcast(&q->readable);
-    pthread_cond_broadcast(&q->writeable);
+    pthread_cond_broadcast(&q->not_empty);
+    pthread_cond_broadcast(&q->not_full);
 }
 
-static void queue_destroy(queue_t *q)
+static inline void queue_destroy(queue_t *q)
 {
-    pthread_cond_destroy(&q->writeable);
-    pthread_cond_destroy(&q->readable);
+    pthread_cond_destroy(&q->not_full);
+    pthread_cond_destroy(&q->not_empty);
     pthread_mutex_destroy(&q->lock);
 
     if (q->data)
@@ -62,45 +62,52 @@ static void queue_destroy(queue_t *q)
     }
 }
 
-static int queue_put(queue_t *q, void *item)
+static inline int queue_put(queue_t *q, void *item)
 {
     pthread_mutex_lock(&q->lock);
-    while ((q->tail + 1) % q->size == q->head)
+    while (1)
     {
-        pthread_cond_wait(&q->writeable, &q->lock);
+        if (q->closed)
+        {
+            pthread_mutex_unlock(&q->lock);
+            return -1;
+        }
+        if ((q->in + 1) % q->size != q->out)
+        {
+            break;
+        }
+        pthread_cond_wait(&q->not_full, &q->lock);
     }
 
-    if (q->closed)
-    {
-        pthread_mutex_unlock(&q->lock);
-        return -1;
-    }
+    q->data[q->in] = item;
+    q->in = (q->in + 1) % q->size;
 
-    q->data[q->tail] = item;
-    q->tail = (q->tail + 1) % q->size;
-
-    pthread_cond_signal(&q->readable);
+    pthread_cond_signal(&q->not_empty);
     pthread_mutex_unlock(&q->lock);
+    return 0;
 }
 
 static void *queue_get(queue_t *q)
 {
     pthread_mutex_lock(&q->lock);
-    while (q->tail == q->head)
+    while (1)
     {
-        pthread_cond_wait(&q->readable, &q->lock);
+        if (q->closed)
+        {
+            pthread_mutex_unlock(&q->lock);
+            return NULL;
+        }
+        if (q->in != q->out)
+        {
+            break;
+        }
+        pthread_cond_wait(&q->not_empty, &q->lock);
     }
 
-    if (q->closed)
-    {
-        pthread_mutex_unlock(&q->lock);
-        return NULL;
-    }
+    void *ret = q->data[q->out];
+    q->out = (q->out + 1) % q->size;
 
-    void *ret = q->data[q->head];
-    q->head = (q->head + 1) % q->size;
-
-    pthread_cond_signal(&q->writeable);
+    pthread_cond_signal(&q->not_full);
     pthread_mutex_unlock(&q->lock);
 
     return ret;
