55a190dd2cc666f23ef785ee3180806d7db672c5
chuqq <chuqingq@qq.com>
2022-11-25 15:54:43 +0800

    improve

--- cpp-util/bounded_blocking_queue.hpp
+++ cpp-util/bounded_blocking_queue.hpp
@@ -8,43 +8,69 @@ class BoundedBlockingQueue
 {
 public:
     explicit BoundedBlockingQueue(int maxSize)
-        : mutex_(),
-          notEmpty_(),
-          notFull_(),
-          queue_(maxSize) // TODO
+        : closed_(false),
+          queue_(new T[maxSize])
     {
     }
 
-    void put(const T &x)
+    ~BoundedBlockQueue()
+    {
+        delete[] queue_;
+    }
+
+    int put(const T &x) // 返回成功失败
     {
         std::unique_lock lock(mutex_);
         notFull_.wait(lock, [&]
                       { return !queue_.full() });
+        if (closed)
+        {
+            return -1;
+        }
         assert(!queue_.full());
         queue_.push_back(x); // TODO
         notEmpty_.notify_one();
     }
 
-    void put(T &&x) // TODO ?? std::forward()
+    int put(T &&x) // TODO ?? std::forward() // 返回成功失败
     {
         std::unique_lock lock(mutex_);
         notFull_.wait(lock, [&]
                       { return !queue_.full() });
+        if (closed)
+        {
+            return -1;
+        }
         assert(!queue_.full());
         queue_.push_back(std::move(x));
         notEmpty_.notify_one();
+        return 0;
     }
 
-    T take()
+    int take(T &v)
     {
         std::unique_lock lock(mutex_);
         notEmpty_.wait(lock, [&]
                        { !queue_.empty() });
+        if (closed)
+        {
+            return -1;
+        }
         assert(!queue_.empty());
-        T front(std::move(queue_.front()));
+        v = std::move(queue_.front()));
         queue_.pop_front();
         notFull_.notify_one();
-        return front;
+        return 0;
+    }
+
+    void close()
+    {
+        {
+            lock_guard<mutex> lk(mtx_);
+            closed_ = true;
+        }
+        not_empty_.notify_all();
+        not_full_.notify_all();
     }
 
     // bool empty() const
@@ -80,4 +106,6 @@ private:
     std::condition_variable notEmpty_;
     std::condition_variable notFull_;
     // boost::circular_buffer<T> queue_ GUARDED_BY(mutex_); // TODO
+    T *queue_;
+    bool closed_;
 };
