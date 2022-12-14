f5c030454f75e3b9ec21a1a255face3e68f12292
chuqq <chuqingq@qq.com>
2022-12-14 15:36:09 +0800

    pubsub shm libuv udp notify 功能ok

---
+++ c++/20221213-pubsub-shm-udp/message.hpp
@@ -0,0 +1,202 @@
+#pragma once
+
+#include <uv.h>
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <set>
+#include <shared_mutex>
+#include <string>
+#include <vector>
+
+#include "shm.hpp"
+
+struct Buffer {
+  char *data_;
+  size_t size_;
+
+  Buffer() : data_(nullptr), size_(0) {}
+  Buffer(char *data, size_t size) : data_(data), size_(size) {}
+};
+
+static void pinger_close_cb(uv_handle_t *handle) {
+  //   std::cout << (void *)handle << " close_cb\n";
+}
+
+class MessagePublisher {
+ public:
+  inline MessagePublisher(const std::string &topic,
+                          uv_loop_t *loop = uv_default_loop());
+  inline int AddSub(const std::string &ip, int port);  // TODO 合并成一个参数
+  inline int Acquire(Buffer *buf);
+  inline void Publish(const Buffer &buf);
+  inline void Stop();
+  inline ~MessagePublisher();
+
+ private:
+  uv_loop_t *loop_;
+  uv_udp_t udp_;
+  std::vector<struct sockaddr_in> subs_;
+  ShmBlock shm_;
+};
+
+const int kBlockNum = 8;
+const int kBlockSize = 4 * 1024 * 1024;
+
+MessagePublisher::MessagePublisher(const std::string &topic, uv_loop_t *loop)
+    : loop_(loop), shm_(topic, kBlockNum, kBlockSize) {
+  int r = uv_udp_init(loop_, &udp_);
+  if (r) {
+    std::cout << "uv_udp_init error\n";
+    throw "uv_udp_init error";
+  }
+  if (!shm_.OpenOrCreate()) {
+    throw "shm OpenOrCreate error";
+  }
+}
+
+int MessagePublisher::AddSub(const std::string &ip, int port) {
+  struct sockaddr_in addr;
+  int r = uv_ip4_addr(ip.c_str(), port, &addr);
+  if (r) {
+    std::cout << "uv_ip4_addr error\n";
+    return -1;
+  }
+  subs_.push_back(addr);
+  return 0;
+}
+
+int MessagePublisher::Acquire(Buffer *buf) {
+  if (buf->size_ > kBlockSize) throw "buf size exceeds kBlockSize";
+  buf->data_ = (char *)shm_.AcquireBlockToWrite();
+  std::cout << "Acquire: " << (void *)buf->data_ << std::endl;
+  buf->size_ = kBlockSize;
+  return 0;
+}
+
+void MessagePublisher::Publish(const Buffer &buf) {
+  std::cout << "publish: " << (void *)buf.data_ << "\n";
+  shm_.ReleaseWrittenBlock(buf.data_);
+  // 通知subs TODO 后面要改成把addr放在State中
+  static char notify = 'n';
+  for (const auto sub : subs_) {
+    uv_buf_t b = uv_buf_init(&notify, sizeof(notify));
+    int r = uv_udp_try_send(&udp_, &b, 1, (const struct sockaddr *)&sub);
+    if (r < 0) {
+      perror("uv_udp_try_send error");
+    }
+    //   std::cout << (void *)this << " pub try_send: " << buf->size_ <<
+    //   std::endl;
+  }
+}
+
+void MessagePublisher::Stop() {
+  uv_close((uv_handle_t *)&udp_, pinger_close_cb);
+  loop_ = nullptr;
+  // shm_会自动析构
+  std::cout << (void *)this << " pub stop\n";
+}
+
+MessagePublisher::~MessagePublisher() {
+  if (loop_) Stop();
+}
+
+class MessageSubscriber {
+ public:
+  typedef std::function<void(MessageSubscriber *sub, const Buffer &buffer)>
+      Callback;
+
+  inline MessageSubscriber(const std::string &topic, const std::string &ip,
+                           int port, const Callback &callback,
+                           uv_loop_t *loop = uv_default_loop());
+  ~MessageSubscriber() {
+    if (loop_) Stop();
+  }
+  inline void Stop();
+
+ private:
+  uv_udp_t udp_;
+  struct sockaddr_in addr_;
+  uv_loop_t *loop_;
+  char buffer_[64 * 1024];
+  Callback callback_;
+  ShmBlock shm_;
+
+  friend inline void buf_alloc(uv_handle_t *tcp, size_t size, uv_buf_t *buf);
+  friend inline void pinger_read_cb(uv_udp_t *udp, ssize_t nread,
+                                    const uv_buf_t *buf,
+                                    const struct sockaddr *addr,
+                                    unsigned flags);
+};
+
+inline void buf_alloc(uv_handle_t *tcp, size_t size, uv_buf_t *buf) {
+  buf->base = ((MessageSubscriber *)tcp->data)->buffer_;
+  buf->len = size;
+}
+
+static void buf_free(const uv_buf_t *buf) {}
+
+inline void pinger_read_cb(uv_udp_t *udp, ssize_t nread, const uv_buf_t *buf,
+                           const struct sockaddr *addr, unsigned flags) {
+  if (nread == 0) {
+    return;
+  }
+  MessageSubscriber *sub = (MessageSubscriber *)udp->data;
+  /* No data here means something went wrong */
+  assert(nread > 0);
+  if (nread > 0) {
+    auto b = sub->shm_.AcquireBlockToRead();
+    Buffer buf((char *)b, kBlockSize);  // TODO 大小未标识，暂时用kBlockSize
+    // std::cout << "read_cb: " << buf->len << "\n";
+    sub->callback_(sub, buf);
+    sub->shm_.ReleaseReadBlock(b);
+  }
+
+  if (buf && !(flags & UV_UDP_MMSG_CHUNK)) buf_free(buf);  // TODO ?
+}
+
+MessageSubscriber::MessageSubscriber(const std::string &topic,
+                                     const std::string &ip, int port,
+                                     const Callback &callback, uv_loop_t *loop)
+    : loop_(loop), callback_(callback), shm_(topic, kBlockNum, kBlockSize) {
+  // udp
+  int r = uv_udp_init(loop_, &udp_);
+  if (r) {
+    std::cout << "uv_udp_init error\n";
+    throw;
+  }
+  udp_.data = this;
+
+  r = uv_ip4_addr(ip.c_str(), port, &addr_);
+  if (r) {
+    std::cout << "uv_ip4_addr error\n";
+  }
+  r = uv_udp_bind(&udp_, (const struct sockaddr *)&addr_, 0);
+  if (r) {
+    perror("bind error");
+    std::cout << "uv_udp_bind error: " << r << std::endl;
+  }
+  r = uv_udp_recv_start(&udp_, buf_alloc, pinger_read_cb);
+  if (r) {
+    std::cout << "uv_udp_recv_start error\n";
+  }
+
+  //   std::cout << (void *)&async_subscriber_ << " subscriber async: " <<
+  //   std::endl;
+  if (!shm_.OpenOrCreate()) {
+    std::cout << "shm_.OpenOrCreate() error\n";
+    throw "shm OpenOrCreate error";
+  }
+}
+
+void MessageSubscriber::Stop() {
+  uv_close((uv_handle_t *)&udp_, pinger_close_cb);
+  loop_ = nullptr;
+  // shm_会自动析构
+  std::cout << "sub stop\n";
+}
---
+++ c++/20221213-pubsub-shm-udp/message_bench.cc
@@ -0,0 +1,98 @@
+#include <assert.h>
+#include <unistd.h>
+
+#include <thread>
+
+#include "message.hpp"
+#include "stop_watch.hpp"
+
+// const uint64_t COUNT = 50000;
+const uint64_t COUNT = 2;
+
+static StopWatch stopwatch;
+
+char value[8] = "\0";
+
+// static Buffer buffer;
+static MessagePublisher *pub1, *pub2;
+static MessageSubscriber *sub1, *sub2;
+
+static inline void Send(MessagePublisher *pub, uint64_t i) {
+  Buffer b;
+  pub->Acquire(&b);
+  *(uint64_t *)b.data_ = i;
+  std::cout << "after acquire: " << (void *)b.data_ << "\n";
+  pub->Publish(b);
+}
+
+void RecvProc(MessageSubscriber *sub, const Buffer &buf) {
+  uint64_t i = *(uint64_t *)buf.data_;  // TODO 转换
+  //   std::cout << "RecvProc: " << i << std::endl;
+  if (i > COUNT) {
+    std::cout << (void *)sub << " sub stopping\n";
+    stopwatch.print(COUNT);
+    sub->Stop();
+    // 本sub停掉后，继续发送，确保另一个线程的sub和pub可以停掉。
+  }
+  MessagePublisher *pub = pub2;
+  auto next = i;
+  if (sub == sub2) {
+    pub = pub1;
+    next += 1;
+  }
+
+  Send(pub, next);
+  // 本pub发送完成后，如果i超过COUNT则停掉。
+  if (i > COUNT) pub->Stop();
+}
+
+int multi_thread_main() {
+  // init
+  const std::string ip("127.0.0.1");
+  const std::string topic1("mytopic1");
+  const std::string topic2("mytopic2");
+  const int port1 = 10001;
+  const int port2 = 10002;
+
+  std::jthread thread2([&] {
+    uv_loop_t loop;
+    uv_loop_init(&loop);
+
+    MessageSubscriber sub11(topic1, ip, port1, RecvProc, &loop);
+    sub1 = &sub11;
+
+    MessagePublisher pub22(topic2, &loop);
+    pub22.AddSub(ip, port2);
+    pub2 = &pub22;
+
+    uv_run(&loop, UV_RUN_DEFAULT);
+    int r = uv_loop_close(&loop);
+    assert(r == 0);
+  });
+
+  MessagePublisher pub11(topic1);
+  pub11.AddSub(ip, port1);
+  pub1 = &pub11;
+  MessageSubscriber sub22(topic2, ip, port2, RecvProc, uv_default_loop());
+  sub2 = &sub22;
+
+  usleep(200);  // TODO
+
+  // 开始测试
+  stopwatch.start();
+
+  Send(pub1, 1);
+  std::cout << "## main thread after Send(1)\n";
+
+  uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+  return 0;
+}
+
+int main() {
+  multi_thread_main();
+  return 0;
+}
+// multi_thread_main: TODO
+// StopWatch: total 1908617835 ns; average 38172 ns/loop.
+// 家中matebook wsl：
+// StopWatch: total 3263650097 ns; average 65273 ns/loop.
