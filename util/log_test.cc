4127b9a88a72e096b96c86d3e8d77ab8d2f1de25
chuqq <chuqingq@qq.com>
2022-12-17 14:58:05 +0800

    c-util和cpp-util合并到util中

--- util/log_test.cc
+++ util/log_test.cc
@@ -1,13 +1,18 @@
 #include "log.hpp"
 
+extern void print_log();
+
 int main() {
-  logd("debug log");
+  logd("debug log %d", 123);
   logi("info log");
   loge("error log");
+  print_log();
 
   SetLogLevel(kLogLevelError);
   logd("debug log");
   logi("info log");
-  loge("error log");
+  loge("error log %f", 1.2);
+
+  print_log();
   return 0;
 }
---
+++ util/test.cpp
@@ -0,0 +1,6 @@
+#include "log.hpp"
+
+void print_log() {
+  logi("123");
+  logd("012");
+}
