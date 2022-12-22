577876c1662d8f26e0cc16c96096ae65eb585e35
chuqq <chuqingq@qq.com>
2022-12-22 16:28:36 +0800

    mempool TODO

---
+++ util/mempool.h
@@ -0,0 +1,19 @@
+// TODO
+
+#ifndef __MPP_MEM_POOL_H__
+#define __MPP_MEM_POOL_H__
+
+typedef struct {
+  int max_block_num;
+  int block_size;
+  std::mutex mutex;
+  std::list<char *> blocks;
+} mempool_t;
+
+mempool_t *mempool_init(mempool_t *pool, int max_block_num, int block_size);
+void mempool_destroy(mempool_t *pool);
+
+char *mempool_alloc(mempool_t *pool);
+void mempool_release(mempool_t *buffer, char *block);
+
+#endif
