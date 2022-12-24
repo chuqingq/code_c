5efda10c0b461a2bab3602c226d1ec074358e24f
chuqq <chuqingq@qq.com>
2022-12-24 16:25:30 +0800

    int+void* 不是lockfree的

---
+++ c++/20221224-atomic/test_atomic.cpp
@@ -0,0 +1,31 @@
+#include <atomic>
+#include <iostream>
+#include <utility>
+
+struct A {
+  int a[100];
+};
+struct B {
+  int x;
+  int y;
+};
+struct C {
+  int x;
+  void *y;
+};
+int main() {
+  std::cout << std::boolalpha << "std::atomic<A> is lock free? "
+            << std::atomic<A>{}.is_lock_free() << '\n'
+            << "std::atomic<B> is lock free? "
+            << std::atomic<B>{}.is_lock_free() << '\n'
+            << "std::atomic<C> is lock free? "
+            << std::atomic<C>{}.is_lock_free() << '\n';
+}
+
+/*
+$ g++ test_atomic.cpp -latomic
+$ ./a.out
+std::atomic<A> is lock free? false
+std::atomic<B> is lock free? true
+std::atomic<C> is lock free? false
+*/
