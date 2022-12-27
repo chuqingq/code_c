7e203191bbdf64de85d9ffc2462f9efdb004eb64
chuqq <chuqingq@qq.com>
2022-12-28 00:33:28 +0800

    format

--- c++/20180427_shared_ptr/test_shared_ptr.cpp
+++ c++/20180427_shared_ptr/test_shared_ptr.cpp
@@ -1,34 +1,33 @@
-#include <memory>
 #include <iostream>
+#include <memory>
 
-struct Base
-{
-    Base() { std::cout << "  Base::Base()\n"; }
-    // Note: non-virtual destructor is OK here
-    ~Base() { std::cout << "  Base::~Base()\n"; }
+struct Base {
+  Base() { std::cout << "  Base::Base()\n"; }
+  // Note: non-virtual destructor is OK here
+  ~Base() { std::cout << "  Base::~Base()\n"; }
 };
 
 int main() {
-    Base *b = NULL;
+  Base *b = NULL;
+  {
+    std::shared_ptr<Base> p = std::make_shared<Base>();
     {
-        std::shared_ptr<Base> p = std::make_shared<Base>();
-        {
-            std::shared_ptr<Base> p2 = p;
-            b = p2.get();
-            printf("b: %p\n", b);
-            std::cout << "leave inner" << std::endl;
-            std::cout << "use_count: " << p.use_count() << std::endl;
-        }
-        std::cout << "leave outer" << std::endl;
-        std::cout << "use_count: " << p.use_count() << std::endl;
-    } // p在这里析构
-    std::cout << "out of outer" << std::endl;
-    // std::cout << "use_count: " << p.use_count() << std::endl;
+      std::shared_ptr<Base> p2 = p;
+      b = p2.get();
+      printf("b: %p\n", b);
+      std::cout << "leave inner" << std::endl;
+      std::cout << "use_count: " << p.use_count() << std::endl;
+    }
+    std::cout << "leave outer" << std::endl;
+    std::cout << "use_count: " << p.use_count() << std::endl;
+  } // p在这里析构
+  std::cout << "out of outer" << std::endl;
+  // std::cout << "use_count: " << p.use_count() << std::endl;
 
-    (void)b; // 这里应该已经是野指针了
-    printf("b: %p\n", b);
-    std::cout << "leave main" << std::endl;
-    return 0;
+  (void)b; // 这里应该已经是野指针了
+  printf("b: %p\n", b);
+  std::cout << "leave main" << std::endl;
+  return 0;
 }
 
 /*
--- c++/20181002_async/test_async.cpp
+++ c++/20181002_async/test_async.cpp
@@ -1,34 +1,35 @@
-//g++ test_async.cpp -std=c++14
+// g++ test_async.cpp -std=c++14
 
 #include <future>
-#include <thread>
 #include <iostream>
+#include <thread>
+
 
 using namespace std::literals;
 
 void f() {
-    std::cout << "start f(): " << std::this_thread::get_id() << std::endl; 
-    std::this_thread::sleep_for(1s);
-    std::cout << "stop" << std::endl;
+  std::cout << "start f(): " << std::this_thread::get_id() << std::endl;
+  std::this_thread::sleep_for(1s);
+  std::cout << "stop" << std::endl;
 }
 
 int main() {
-    std::cout << "main: " << std::this_thread::get_id() << std::endl;
-    std::this_thread::sleep_for(100ms);
+  std::cout << "main: " << std::this_thread::get_id() << std::endl;
+  std::this_thread::sleep_for(100ms);
 
-    auto fut = std::async(f);
+  auto fut = std::async(f);
 
-    if (fut.wait_for(0ms) == std::future_status::deferred) {
-        std::cout << "f被推迟执行" << std::endl;
-        return -1;
-    }
+  if (fut.wait_for(0ms) == std::future_status::deferred) {
+    std::cout << "f被推迟执行" << std::endl;
+    return -1;
+  }
 
-    while (fut.wait_for(100ms) != std::future_status::ready) {
-        std::cout << "!=" << std::endl;
-    }
+  while (fut.wait_for(100ms) != std::future_status::ready) {
+    std::cout << "!=" << std::endl;
+  }
 
-    std::cout << "end" << std::endl;
-    return 0;
+  std::cout << "end" << std::endl;
+  return 0;
 }
 // 没有大压力时会过一会儿就结束；有大压力时可能返回 std::future_status::deferred
 /*
