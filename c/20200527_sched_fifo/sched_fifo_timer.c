1c769deec1560c74c0651e11c47db717046b1df6
chuqq <chuqingq@qq.com>
2023-02-04 16:59:23 +0800

    sched_fifo_timer

--- c/20200527_sched_fifo/sched_fifo_timer.c
+++ c/20200527_sched_fifo/sched_fifo_timer.c
@@ -19,6 +19,10 @@ static unsigned long long nstime(void) {
     return ((unsigned long long)ts.tv_sec)*1e9 + ts.tv_nsec;                    
 }
 
+void nspause() {
+        __asm__ __volatile__("pause;");
+}
+
 pthread_t createSchedFifoThread(void* (*pThreadFunc)(void*), int priority) {
     struct sched_param param;
     int policy;
@@ -29,37 +33,49 @@ pthread_t createSchedFifoThread(void* (*pThreadFunc)(void*), int priority) {
         perror("pthread_create() error:");
         return 0;
     }
-    // param.sched_priority = priority;
-    // policy = SCHED_FIFO;
-    // ret = pthread_setschedparam(id, policy, &param);
-    // if(ret != 0)
-    // {
-    //     perror("pthread_setschedparam() error:");
-    //     return 0;
-    // }
+    param.sched_priority = priority;
+    policy = SCHED_FIFO;
+    ret = pthread_setschedparam(id, policy, &param);
+    if(ret != 0)
+    {
+        perror("pthread_setschedparam() error:");
+        return 0;
+    }
     return id;
 }
 
 void* thread1(void* arg) {
     unsigned long long loop = 1; // 循环次数
-    struct timeval tv;
-    unsigned long long start = ustime();
+    struct timespec tv;
+    tv.tv_sec = 0;
+    // unsigned long long start = nstime();
+    unsigned long long start = nstime();
+    printf("====start: %llu\n", start);
     unsigned long long target;
     unsigned long long now;
+    long long d;
     while(1) {
-        target = start + loop*1000;
-        // 模拟业务开始
-        usleep(loop % 500); // 模拟500us以内的业务
+        now = nstime();
         if (loop++ % 1000 == 0) {
             printf("%lld\n", now);
         }
+
+        // 模拟业务开始
+        usleep(loop % 500); // 模拟500us以内的业务
         // 模拟业务结束
-        now = ustime();
-        tv.tv_sec = 0;
-        tv.tv_usec = target - now;
+
+        target = start + loop*1e6; // 提前睡醒，再自旋
+        // tv.tv_nsec = target - now;
         // select(0, NULL, NULL, NULL, &tv);
         // nanosleep(next - nstime());
-        usleep((target > now) ? (target - now) : 0);
+        // if (target > now) {
+        //     nanosleep(&tv, NULL);
+        // }
+        //  else {
+        //     printf("target <= now: %lld\n", now - target);
+        // }
+        while ((d = (target-80*1e3-nstime())) > 0) usleep(d/1e3); // pause?
+        while (nstime() < target) nspause(); // pause?
     }
 }
 
@@ -74,30 +90,31 @@ int main(void)
 }
  
 /*
-gcc sched_fifo.c -pthread
-sudo taskset -c 0 ./a.out
+gcc -static -o test_sched sched_fifo_timer_ns_final.c -pthread
+sudo taskset -c 0 ./test_sched
 
 结论：
-1.在没有实时线程的情况下，代码写好，可以控制到每1ms触发一次，误差是50ns以内。
-2.用select和usleep、nanosleep精度差不多。说是select对于信号、中断等更安全
-3.没有验证高负载情况。猜测高负载下精度应该会受影响，可以通过上实时线程来解决。
+1.using sudo ./test_sched (with fifo sched), jitter is less than 1ns. CPU usage is about 5% in one core(top).
+(with 'stress -c 10', total cpu 100%,  same result)
+2.在没有实时线程的情况下，代码写好（可以先usleep降低cpu，然后提前醒来进行自选），可以控制到每1ms触发一次，误差是100ns以内。
+(with 'stress -c 10', jitter is less than 1ns....???)
+3.用select和usleep、nanosleep精度差不多。说是select对于信号、中断等更安全
+4.没有验证高负载情况。猜测高负载下精度应该会受影响，可以通过上实时线程来解决。
+5.pause no effect, still 5% cpu usage.
 
 结果：
-chuqq@chuqq-r7000p ~/w/t/p/c/c/20200527_sched_fifo (master)> ./a.out
-1639047142393492
-1639047143393490
-1639047144393488
-1639047145393485
-1639047146393495
-1639047147393481
-1639047148393487
-1639047149393487
-1639047150393484
-1639047151393487
-1639047152393488
-1639047153393485
-1639047154393531
-1639047155393492
+chuqq@chuqq-r7000p/m/d/t/p/c/c/20200527_sched_fifo $ sudo taskset -c 0 ./a.out
+====start: 1665541206242488832
+1665541207242488832
+1665541208242488832
+1665541209242488832
+1665541210242488832
+1665541211242488832
+1665541212242488832
+1665541213242488832
+1665541214242488832
 
+cpu占用率：单核7%
+    PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND
+   3621 root      20   0   76212    880    788 S   7.0   0.0   0:01.82 a.out
 */
-
