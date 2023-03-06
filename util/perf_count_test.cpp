ce69a835579b8a7d62e901f3336372a26995a099
chuqq <chuqingq@qq.com>
2023-03-06 16:02:49 +0800

    补充测试输出

--- util/perf_count_test.cpp
+++ util/perf_count_test.cpp
@@ -10,6 +10,10 @@ int main() {
     return 0;
 }
 /*
-g++ -o perf_count_test{,.cpp} -g -Wall
-./perf_count_test
+$ g++ -o perf_count_test{,.cpp} -g -Wall
+$ ./perf_count_test
+PerfCount: total 1000175597 ns; average 1093088 ns/loop.
+PerfCount: total 1000342916 ns; average 1093270 ns/loop.
+PerfCount: total 1000148143 ns; average 1093058 ns/loop.
+PerfCount: total 1000165898 ns; average 3922219 ns/loop.
 */
