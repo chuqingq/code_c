e16fb7f20661d2ffe003df28a8c2a1c7ad1722db
chuqq <chuqingq@qq.com>
2023-03-31 09:56:07 +0800

    stop_watch

--- util/stop_watch.hpp
+++ util/stop_watch.hpp
@@ -5,19 +5,25 @@
 
 class StopWatch {
 public:
-  StopWatch() { start(); }
+  StopWatch() { Start(); }
 
-  void start() { start_ = std::chrono::steady_clock::now(); }
+  void Start() {
+    start_ = std::chrono::steady_clock::now();
+    loops_ = 0;
+  }
 
-  void print(uint64_t loops) {
+  void AddLoops(uint64_t loops = 1) { loops_ += loops; }
+
+  void StopAndStats() {
     auto stop = std::chrono::steady_clock::now();
     auto ns =
         std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start_)
             .count();
-    std::cout << "StopWatch: total " << ns << " ns; average " << ns / loops
+    std::cout << "StopWatch: duration " << ns << " ns; loops: " << loops_ << "; average " << ns / loops_
               << " ns/loop.\n";
   }
 
 private:
   std::chrono::time_point<std::chrono::steady_clock> start_;
+  uint64_t loops_;
 };
--- util/stop_watch_test.cpp
+++ util/stop_watch_test.cpp
@@ -1,15 +1,23 @@
 #include "stop_watch.hpp"
 
+#include <chrono>
 #include <thread>
+#include <unistd.h>
 
 int main() {
-  StopWatch w;
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  w.print(1);
+  StopWatch watch;
+
+  for (auto i = 0; i < 10; i ++) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    watch.AddLoops(1);
+  }
+  
+  watch.StopAndStats();
   return 0;
 }
 
 /*
-$ g++ -o {stop_watch_test_cpp,.cpp} -Wall
-$ ./stop_watch_test_cpp
+$ g++ -o stop_watch_test{,.cpp} -Wall -g
+$ ./stop_watch_test
+StopWatch: duration 1001040897 ns; loops: 10; average 100104089 ns/loop.
 */
