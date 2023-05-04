d35c88147a2a3856c9d6c614989e44b58a543122
chuqq <chuqingq@qq.com>
2023-05-04 16:38:10 +0800

    string_view.cpp格式化

--- c++/20230503-string_view/string_view.cpp
+++ c++/20230503-string_view/string_view.cpp
@@ -2,18 +2,19 @@
 #include <string_view>
 
 int main() {
-    std::string str("123456123");
+  std::string str("123456123");
 
-    std::string_view sv1(&str[0], 3);
-    std::string_view sv2(&str[6], 3);
-    std::string_view sv3(str.substr(0, 3));
+  std::string_view sv1(&str[0], 3);
+  std::string_view sv2(&str[6], 3);
+  std::string_view sv3(str.substr(0, 3));
 
-    printf("str: %p\n", str.data());
-    printf("end: %p\n", str.data()+str.size());
-    printf("sv1: %p\n", sv1.data());
-    printf("sv2: %p\n", sv2.data());
-    printf("sv3: %p\n", sv3.data());
-    return 0;
+  printf("str: %p\n", str.data());
+  printf("end: %p\n", str.data() + str.size());
+  printf("sv1: %p\n", sv1.data());
+  printf("sv2: %p\n", sv2.data());
+  printf("sv3: %p\n", sv3.data());
+
+  return 0;
 }
 /*
 $ g++ string_view.cpp -Wall -std=c++17
