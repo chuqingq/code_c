e0a2123009a1f74cfb2a78df6737a8db45f4fcd9
chuqq <chuqingq@qq.com>
2023-11-06 06:33:55 +0000

    defer.hpp基于scope_guard.hpp

--- util/defer.hpp
+++ util/defer.hpp
@@ -1,18 +1,19 @@
 #pragma once
 
-template <typename Fn>
-class Defer {
-  const Fn &fn_;
+#include "scope_guard.hpp"
+// template <typename Fn>
+// class Defer {
+//   const Fn &fn_;
 
- public:
-  Defer(const Fn &fn) noexcept : fn_(fn) {}
+//  public:
+//   Defer(const Fn &fn) noexcept : fn_(fn) {}
 
-  ~Defer() noexcept { fn_(); }
+//   ~Defer() noexcept { fn_(); }
 
-  Defer(Defer &&) = delete;
-};
+//   Defer(Defer &&) = delete;
+// };
 
 #define DEFER_PASTE_(x, y) x##y
 #define DEFER_CAT_(x, y) DEFER_PASTE_(x, y)
-#define DEFER(...) \
-  Defer DEFER_CAT_(defer_, __LINE__) { __VA_ARGS__ }
+#define DEFER(...)                                                             \
+  ScopeGuard DEFER_CAT_(defer_, __LINE__) { __VA_ARGS__ }
